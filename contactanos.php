0c7198b203d169e9dfdf0839816ae58172faa15f
maximiliano <maximilianojlopez@hotmail.com>
2024-02-16 18:22:55 -0300

    primer commit

---
+++ contactanos.php
@@ -0,0 +1,334 @@
+<?php include ("admin/bd.php");
+include("carrito_de_compras/carrito.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHP-mailer/Exception.php';
+require 'PHP-mailer/PHPMailer.php';
+require 'PHP-mailer/SMTP.php';
+
+
+$errores = '';
+$enviado = '';
+
+// Comprobamos que el formulario haya sido enviado.
+if (isset($_POST['submit'])) {
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$mensaje = $_POST['mensaje'];
+    $asunto = $_POST['asunto'];
+
+// Comprobamos que el nombre no este vacio.
+	if (!empty($nombre)) {
+
+		// Saneamos el nombre para eliminar caracteres que no deberian estar.
+		$nombre = trim($nombre);
+		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		
+        // Comprobamos que el nombre despues de quitar los caracteres ilegales no este vacio.
+		if ($nombre == "") {
+			$errores.= 'Por favor ingresa un nombre.<br />';
+		}
+	} else {
+		$errores.= 'Por favor ingresa un nombre.<br />';
+	}
+
+	if (!empty($correo)) {
+		$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+		// Comprobamos que sea un correo valido
+		if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+			$errores.= "Por favor ingresa un correo valido.<br />";
+		}
+	} else {
+		$errores.= 'Por favor ingresa un correo.<br />';
+	}
+
+
+	if (!empty($mensaje)) {
+		// Podemos sanear la cadena de texto con filter_var, pero queremos que en el mensaje los signos se conviertan en entidades HTML
+		$mensaje = htmlspecialchars($mensaje);
+		$mensaje = trim($mensaje);
+		$mensaje = stripslashes($mensaje);
+	} else {
+		$errores.= 'Por favor ingresa el mensaje.<br />';
+	}
+
+// Comprobamos si hay errores, si no hay entonces enviamos.
+	if (!$errores) {
+		
+		//Create an instance; passing `true` enables exceptions
+		$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'maximilianojlopez@hotmail.com';                     //SMTP username
+    $mail->Password   = 'mailen13082019';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('maximilianojlopez@hotmail.com', 'Maxi');
+    $mail->addAddress($correo);     //Add a recipient
+    
+    
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $asunto;
+    $mail->Body    = $mensaje;
+    $mail->send();
+
+    
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+		
+	}
+}
+if($_POST){
+  
+	//recepcionamos los valores del formulario
+	$nombre=(isset($_POST['nombre']))?$_POST['nombre']:"";
+	$asunto=(isset($_POST['asunto']))?$_POST['asunto']:"";
+	$correo=(isset($_POST['correo']))?$_POST['correo']:"";
+    $mensaje=(isset($_POST['mensaje']))?$_POST['mensaje']:"";
+
+	$sentencia= $conexion->prepare("INSERT INTO `tbl_mensajes` 
+	(`nombre`, `asunto`, `correo`, `mensaje`) 
+	VALUES (:nombre, :asunto, :correo, :mensaje);");
+	
+	$sentencia->bindParam(":nombre",$nombre);
+	$sentencia->bindParam(":asunto",$asunto);
+	$sentencia->bindParam(":correo",$correo);
+    $sentencia->bindParam(":mensaje",$mensaje);
+	
+	$sentencia->execute();  
+  
+  
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>KOPPEE - Coffee Shop HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+       <!-- Navbar Start -->
+       <div class="container-fluid p-0 nav-bar">
+        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+            <a href="index.php" class="navbar-brand px-lg-4 m-0">
+                <h1 class="m-0 display-4 text-uppercase text-white">KOPPEE</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav ml-auto p-4">
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
+                    <a href="nosotros.php" class="nav-item nav-link">Nosotros</a>
+                    <a href="servicios.php" class="nav-item nav-link">Servicios</a>
+                    <a href="menu.php" class="nav-item nav-link">Menu</a>    
+                    <a href="reservas.php" class="nav-item nav-link">Reservaciones</a>
+                    <a href="testimonios.php" class="nav-item nav-link">Testimonios</a>
+                    <a href="contactanos.php" class="nav-item nav-link">Contactanos</a>
+                    <a href="login/index.php" class="nav-item nav-link">
+                        <img width="50" class="rounded-circle mb-3 mb-sm-0"  src="img/fotos/images12.png">
+                    </a>    
+                    <?php if($mensaje!=""){?>
+                    <a href="carrito_de_compras/index.php" class="nav-item nav-link">Carrito (<?php
+                    echo(empty($_SESSION['carrito']))?0:count($_SESSION['carrito']);
+                    ?>)</a><?php }?>
+                </div>
+            </div>  
+        </nav>
+        <div class="container">
+            <?php $mensaje; ?>
+        </div>
+    </div>
+    </div>
+
+    <!-- Navbar End -->
+
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header mb-5 position-relative overlay-bottom">
+        <div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style="min-height: 400px">
+            <h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Contact</h1>
+            <div class="d-inline-flex mb-lg-5">
+                <p class="m-0 text-white"><a class="text-white" href="">Home</a></p>
+                <p class="m-0 text-white px-2">/</p>
+                <p class="m-0 text-white">Contact</p>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+
+    <!-- Contact Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="section-title">
+                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Contact Us</h4>
+                <h1 class="display-4">Feel Free To Contact</h1>
+            </div>
+            <div class="row px-3 pb-2">
+                <div class="col-sm-4 text-center mb-3">
+                    <i class="fa fa-2x fa-map-marker-alt mb-3 text-primary"></i>
+                    <h4 class="font-weight-bold">Address</h4>
+                    <p>123 Street, New York, USA</p>
+                </div>
+                <div class="col-sm-4 text-center mb-3">
+                    <i class="fa fa-2x fa-phone-alt mb-3 text-primary"></i>
+                    <h4 class="font-weight-bold">Phone</h4>
+                    <p>+012 345 6789</p>
+                </div>
+                <div class="col-sm-4 text-center mb-3">
+                    <i class="far fa-2x fa-envelope mb-3 text-primary"></i>
+                    <h4 class="font-weight-bold">Email</h4>
+                    <p>info@example.com</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 pb-5">
+                    <iframe style="width: 100%; height: 443px;"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+                <div class="col-md-6 pb-5">
+                    <div class="contact-form">
+                        <div id="success"></div>
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                            <div class="control-group">
+                                <input type="text" name="nombre" id="nombre" class="form-control bg-transparent p-4"  placeholder="Your Name" value="<?php if(!$enviado && isset($nombre))?>"
+                                    required="required" data-validation-required-message="Please enter your name" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <input type="email" name="correo" id="correo" class="form-control bg-transparent p-4"  placeholder="Your Email" value="<?php if(!$enviado && isset($correo))?>"
+                                    required="required" data-validation-required-message="Please enter your email" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <input type="text" name="asunto" id="asunto" class="form-control bg-transparent p-4"  placeholder="Subject" value="<?php if(!$enviado && isset($asunto))?>"
+                                    required="required" data-validation-required-message="Please enter a subject" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <textarea name="mensaje" id="mensaje" class="form-control bg-transparent py-3 px-4" rows="5"  placeholder="Message"value="<?php if(!$enviado && isset($mensaje))?>"
+                                    required="required"
+                                    data-validation-required-message="Please enter your message"></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div>
+                                <button type="submit" name="submit" class="btn btn-primary font-weight-bold py-3 px-5">Enviar mensaje</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid footer text-white mt-5 pt-5 px-0 position-relative overlay-top">
+        <div class="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Get In Touch</h4>
+                <p><i class="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
+                <p><i class="fa fa-phone-alt mr-2"></i>+012 345 67890</p>
+                <p class="m-0"><i class="fa fa-envelope mr-2"></i>info@example.com</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Follow Us</h4>
+                <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
+                <div class="d-flex justify-content-start">
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Open Hours</h4>
+                <div>
+                    <h6 class="text-white text-uppercase">Monday - Friday</h6>
+                    <p>8.00 AM - 8.00 PM</p>
+                    <h6 class="text-white text-uppercase">Saturday - Sunday</h6>
+                    <p>2.00 PM - 8.00 PM</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Newsletter</h4>
+                <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
+                <div class="w-100">
+                    <div class="input-group">
+                        <input type="text" class="form-control border-light" style="padding: 25px;" placeholder="Your Email">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary font-weight-bold px-3">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
+            <p class="mb-2 text-white">Copyright &copy; <a class="font-weight-bold" href="#">Domain</a>. All Rights Reserved.</a></p>
+            <p class="m-0 text-white">Designed by <a class="font-weight-bold" href="https://htmlcodex.com">HTML Codex</a></p>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
